e73aafc17ef57ece29aaa2b18665eb75d593d378
Vince Salvino <salvino@coderedcorp.com>
2022-10-05 15:38:09 -0400

    Add project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Django Demo on CodeRed Cloud
+
+This is a simple Django app which runs on CodeRed Cloud and is automatically deployed via GitHub Actions.
---
+++ demo_django/settings/prod.py
@@ -0,0 +1,27 @@
+from .base import *  # noqa
+import os
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = [os.environ["VIRTUAL_HOST"]]
+
+SECRET_KEY = os.environ["RANDOM_SECRET_KEY"]
+
+# Built-in email sending service provided by CodeRed Cloud.
+# Change this to a different backend or SMTP server to use your own.
+EMAIL_BACKEND = "django_sendmail_backend.backends.EmailBackend"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": os.environ["DB_HOST"],
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "OPTIONS": {
+            "ssl": {},
+            "charset": "utf8mb4",
+        },
+    }
+}
